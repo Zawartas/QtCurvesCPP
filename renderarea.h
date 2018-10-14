814e8264ac008e4d530df55e041a2491bfdefdc0
Zawartas <zawartas@gmail.com>
2018-10-14 13:02:35 +0200

    Implementing Render Area

---
+++ renderarea.h
@@ -0,0 +1,17 @@
+#ifndef RENDERAREA_H
+#define RENDERAREA_H
+
+#include <QWidget>
+
+class renderArea : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit renderArea(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // RENDERAREA_H
